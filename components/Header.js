3c09fb9e01a90bf6aa99cb4a45b40a8baf8400e3
Uttam Marandi <uttammarandi007@gmail.com>
2021-10-04 11:35:05 +0530

    next js with talwind

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="header">
+      {/* left */}
+      <div>
+        <Image />
+      </div>
+      {/* middle */}
+      <div></div>
+      {/* right */}
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
