c7a2cbbb39560370062085177dbfab0a2e0084e8
Uttam Marandi <uttammarandi007@gmail.com>
2021-10-04 22:25:18 +0530

    header complete

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,44 @@
 import React from "react";
 import Image from "next/image";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
       {/* left */}
-      <div>
-        <Image />
+      <div className="relative flex items-center h-10 cursor-pointer">
+        <Image
+          src="https://dev-sachitstudio.pantheonsite.io/wp-content/uploads/2021/10/airbnb_logo.png"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
       </div>
       {/* middle */}
-      <div></div>
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
       {/* right */}
-      <div></div>
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["dev-sachitstudio.pantheonsite.io"],
+  },
+};
